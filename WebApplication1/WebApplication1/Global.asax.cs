b73051c9e410d3bb1faec7a9da9415d83fddf02c
jiean <hjiean@hotmail.com>
2015-11-05 13:24:35 -0600

    2nd commit

--- WebApplication1/WebApplication1/Global.asax.cs
+++ WebApplication1/WebApplication1/Global.asax.cs
@@ -10,7 +10,7 @@ namespace WebApplication1
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-        protected void Application_Start()
+        protected void Application_Start()//
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
